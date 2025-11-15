21b37bb9a26fbaa39e6d93c8c0eeb21358fd5a14
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2025-11-15 13:01:20 +0530

    remove cpu benchmarks

--- enca/benches/bench_nca.rs
+++ enca/benches/bench_nca.rs
@@ -8,57 +8,29 @@ use itertools::Itertools;
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
 
-fn single_grid_small(c: &mut Criterion) {
+fn single_grid(c: &mut Criterion) {
     let mut rng = ChaCha8Rng::seed_from_u64(1);
     let tasks_path = "./data/v1/arc-agi_training_challenges.json";
     let dataset = Dataset::load(&tasks_path, None);
 
-    let task = &dataset.get_task("794b24be").unwrap(); // 3x2 grid
+    let grid_small = &dataset.get_task("794b24be").unwrap().train[0].input; // 3x3 grid
+    let grid_large = &dataset.get_task("264363fd").unwrap().train[0].input; // 30x30 grid
 
     let mut nca = NCA::new(100);
     nca.initialize_random(&mut rng);
 
-    let mut group = c.benchmark_group("nca_single_grid_small");
+    let mut group = c.benchmark_group("nca_single_grid");
 
-    group.bench_function("gpu", |b| {
+    group.bench_function("small", |b| {
         b.iter(|| {
-            let mut executor = NCAExecutor::new(nca.clone(), &task.train[0].input, Backend::GPU);
+            let mut executor = NCAExecutor::new(nca.clone(), &grid_small, Backend::GPU);
             executor.run();
         })
     });
 
-    group.bench_function("cpu", |b| {
+    group.bench_function("large", |b| {
         b.iter(|| {
-            let mut executor = NCAExecutor::new(nca.clone(), &task.train[0].input, Backend::CPU);
-            executor.run();
-        })
-    });
-
-    group.finish();
-}
-
-fn single_grid_largest(c: &mut Criterion) {
-    let mut rng = ChaCha8Rng::seed_from_u64(1);
-    let tasks_path = "./data/v1/arc-agi_training_challenges.json";
-    let dataset = Dataset::load(&tasks_path, None);
-
-    let task = &dataset.get_task("264363fd").unwrap(); // 30x30 grid
-
-    let mut nca = NCA::new(100);
-    nca.initialize_random(&mut rng);
-
-    let mut group = c.benchmark_group("nca_single_grid_largest");
-
-    group.bench_function("gpu", |b| {
-        b.iter(|| {
-            let mut executor = NCAExecutor::new(nca.clone(), &task.train[0].input, Backend::GPU);
-            executor.run();
-        })
-    });
-
-    group.bench_function("cpu", |b| {
-        b.iter(|| {
-            let mut executor = NCAExecutor::new(nca.clone(), &task.train[0].input, Backend::CPU);
+            let mut executor = NCAExecutor::new(nca.clone(), &grid_large, Backend::GPU);
             executor.run();
         })
     });
@@ -92,19 +64,19 @@ fn multi_grid(c: &mut Criterion) {
         })
     });
 
-    group.bench_function("cpu", |b| {
-        b.iter(|| {
-            for nca in &ncas {
-                for grid in &grids {
-                    let mut executor = NCAExecutor::new(nca.clone(), grid, Backend::CPU);
-                    executor.run();
-                }
-            }
-        })
-    });
+    // group.bench_function("cpu", |b| {
+    //     b.iter(|| {
+    //         for nca in &ncas {
+    //             for grid in &grids {
+    //                 let mut executor = NCAExecutor::new(nca.clone(), grid, Backend::CPU);
+    //                 executor.run();
+    //             }
+    //         }
+    //     })
+    // });
 
     group.finish();
 }
 
-criterion_group!(benches, single_grid_small, single_grid_largest, multi_grid);
+criterion_group!(benches, single_grid, multi_grid);
 criterion_main!(benches);
